e07cd34603f31b00cf93e0ad4aed91f97903c93c
Fovir <fovir@disroot.org>
2025-11-13 13:43:24 +0800

    plugins/abolish: init.

---
+++ tests/test-sources/plugins/by-name/abolish/default.nix
@@ -0,0 +1,19 @@
+_: {
+  empty = {
+    plugins.abolish = {
+      enable = true;
+    };
+  };
+
+  example = {
+    plugins.abolish = {
+      enable = true;
+    };
+  };
+
+  defaults = {
+    plugins.abolish = {
+      enable = true;
+    };
+  };
+}
